3be301f2152d3cb0f8fc7783892c85e7ff0a670d
Higor Anjos <higorc.anjos@gmail.com>
2023-03-24 11:01:56 +0000

    build: express + lint

---
+++ routes/index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+// const swaggerUi = require('swagger-ui-express');
+// const swaggerJSDoc = require('swagger-jsdoc');
+// const swaggerConfig = require('../docs/swagger.config');
+
+require('express-async-errors');
+const user = require('./user');
+
+const root = express.Router({ mergeParams: true });
+
+/**
+ * @swagger
+ *  tags:
+ *    name: User
+ *    description: Rotas de usuario
+ */
+root.use('/user', user);
+
+// const swaggerDoc = swaggerJSDoc(swaggerConfig);
+// root.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDoc));
+
+root.get('/', (_req, res) => res.send({ message: 'Hello World' }));
+
+root.use((error, _req, res, _next) => {
+  res.status(error.status || 500);
+  res.json({
+    error: {
+      message: error.message,
+    },
+  });
+});
+
+module.exports = root;
---
+++ routes/user.js
@@ -0,0 +1,9 @@
+const express = require('express');
+// const auth = require('../middlewares/auth');
+// const { login, create, update, remove } = require('../controller/user');
+
+const router = express.Router({ mergeParams: true });
+
+// router.post('/login', login);
+
+module.exports = router;
