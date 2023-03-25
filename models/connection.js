59832363ac65703dfd6c39bc22fec28e25a0e5bd
Higor Anjos <higorc.anjos@gmail.com>
2023-03-25 15:36:49 -0300

    feat: conexao com o banco

--- models/connection.js
+++ models/connection.js
@@ -4,9 +4,10 @@ require('dotenv').config();
 const connection = mysql.createPool({
   host: process.env.DB_HOST || 'localhost',
   user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASS || 'sua_senha',
+  password: process.env.DB_PASSWORD || 'sua_senha',
   database: process.env.DB_NAME || 'model_example',
   port: process.env.DB_PORT || 3306,
+  ssl: {}, // adicionado para estabelecer uma conexão segura
 });
 
 module.exports = connection;
