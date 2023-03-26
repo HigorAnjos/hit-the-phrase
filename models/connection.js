b31b2cc953773b4c0444d6bdaf67c2a5fba6be04
higor cavalcante dos anjos <higorc.anjos@gmail.com>
2023-03-26 18:34:35 -0300

    fix: credenciais exposta

--- models/connection.js
+++ models/connection.js
@@ -2,10 +2,10 @@ const mysql = require('mysql2/promise');
 require('dotenv').config();
 
 const connection = mysql.createPool({
-  host: process.env.DB_HOST || 'localhost',
-  user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASS || 'sua_senha',
-  database: process.env.DB_NAME || 'model_example',
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
   port: process.env.DB_PORT || 3306,
   ssl: {}, // adicionado para estabelecer uma conexão segura
 });
