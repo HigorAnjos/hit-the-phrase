da24785dd9bde9bff90c12a47096481710692b1b
higor cavalcante dos anjos <higorc.anjos@gmail.com>
2023-04-18 23:51:02 -0300

    fix(card): newphrase

--- models/cards/create.models.js
+++ models/cards/create.models.js
@@ -2,9 +2,15 @@ const connection = require('../connection');
 
 const create = async (userId, idPhrase) => {
   // const query = 'INSERT INTO user_frase (idUser, idFrase, `like`) VALUES (?, ?, 0)';
-  const query = 'INSERT INTO user_frase (idUser, idFrase, `like`) SELECT ?, ?, 0 FROM dual WHERE NOT EXISTS ( SELECT 1 FROM user_frase WHERE idUser = ? AND idFrase = ?);';
+  const querySelect = 'SELECT 1 FROM user_frase WHERE idUser = ? AND idFrase = ?';
 
-  const [result] = await connection.execute(query, [userId, idPhrase, userId, idPhrase]);
+  const [resultSelect] = await connection.execute(querySelect, [userId, idPhrase]);
+
+  if (resultSelect.length > 0) return null;
+
+  const query = 'INSERT INTO user_frase (idUser, idFrase, `like`) VALUES (?, ?, 0)';
+
+  const [result] = await connection.execute(query, [userId, idPhrase]);
 
   if (result.affectedRows === 0) return null;
 
